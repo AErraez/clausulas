d2883a8baab1797303be9631bb9829da959fdd42
Ariel Erraez <arielon88@gmail.com>
2023-04-19 00:14:29 -0500

    buttonssss

--- main.js
+++ main.js
@@ -7,14 +7,25 @@ const stringSimilarity = require('string-similarity');
 function findSimilarSentences(objclaus, inputSentence) {
   let similarities = [];
   let sentencesArray=objclaus.map(claus=>claus["Cláusula"])
+  let codearray= objclaus.map(claus=>claus["Código"])
   for (let i = 0; i < sentencesArray.length; i++) {
-    var sentence = sentencesArray[i];
-    sentence=sentence.toLowerCase()
+    var sentence = sentencesArray[i]
+    var codclaus= codearray[i]
+    sentence=sentence.toUpperCase()
     const similarityScore = Number(stringSimilarity.compareTwoStrings(sentence, inputSentence)).toFixed(2);
-    similarities.push({sentence, similarityScore});
+    similarities.push({sentence, similarityScore, codclaus});
   }
   similarities.sort((a, b) => b.similarityScore - a.similarityScore);
-  return similarities.slice(0, 5).map(similarity => similarity.sentence +" "+ similarity.similarityScore);
+  return similarities.slice(0, 5).map(similarity => [similarity.sentence, similarity.codclaus, similarity.similarityScore])
+}
+
+function arrtotext(arr) {
+  let unique= [ ...new Set(arr)]
+  let txt= ""
+  unique.forEach(elem=> {
+    txt+= elem+ "-"
+  })
+  return txt.substring(0,txt.length -1)
 }
 
 let boton= document.getElementById("submit")
@@ -27,6 +38,7 @@ boton.addEventListener("click", (event)=>{
     let txtclean= arrtxt.map((elem)=> {   //Se limpia el texto ingresado por el usuario
         elem= elem.replace("-"," ")
         elem= elem.trim()
+        elem= elem.toUpperCase()
         return elem
     })
 
@@ -34,14 +46,53 @@ boton.addEventListener("click", (event)=>{
     console.log(txtclean[0])
     let section=document.getElementById("resultado")
     section.innerHTML=""
+    let codarray=[]
     txtclean.forEach(element => {
       let similist = findSimilarSentences(clausramo,element)
-      let txttoshow= `<div class="col-5"><p>${element} ===> </p></div> 
-                      <div class="col-2"><p>${similist[0]}</p></div> 
-                      <div class="col-2"><p>${similist[1]}</p></div> 
-                      <div class="col-2"><p>${similist[2]} </p></div>
-                      <div class="col-1"><p>X</p></div>`
+      let txttoshow= `<div class="row py-1 justify-content-around align-items-center text-align-center">
+                      <div class="col-5"><p>${element} ===> </p></div> 
+                      <div class="col-2 "><button class="claus-but" value="active">${similist[0][0]} - ${similist[0][1]}</button></div> 
+                      <div class="col-2 "><button class="claus-but" value="inactive">${similist[1][0]} - ${similist[1][1]}</button></div> 
+                      <div class="col-2 "><button class="claus-but" value="inactive">${similist[2][0]} - ${similist[2][1]}</button></div> 
+                      <div class="col-md-auto text-center"><button class="claus-but" value="inactive">X</button></div> </div>`
       section.innerHTML+=txttoshow
-    });
+      codarray.push(similist[0][1])
+    })
+    let codtxt= arrtotext(codarray)
+    section.innerHTML+=`<div class="row py-4 justify-content-around align-items-center text-align-center"> ${codtxt}</div>`
+
+    let listbut=document.querySelectorAll(".claus-but")
+    let ibut=0
+    let iarr=0
+    let arrbut=[]
+    let objbut={}
+    for (let but of listbut) {
+      arrbut.push({but})
+      
+      if( ibut>0 && ibut%4==3){
+        objbut[iarr.toString()]=arrbut
+        arrbut=[]
+        iarr+=1
+      }
+      ibut+=1
+
+    }
+    console.log(objbut)
+
+    for (let fila_boton in objbut){
+      let boton1=objbut[fila_boton][0]
+      let boton2=objbut[fila_boton][1]
+      let boton3=objbut[fila_boton][2]
+      let boton4=objbut[fila_boton][3]
+      for (let boton of objbut[fila_boton]){
+        boton.but.addEventListener("click", (event)=>{
+          boton1.but.value="inactive"
+          boton2.but.value="inactive"
+          boton3.but.value="inactive"
+          boton4.but.value="inactive"
+          boton.but.value="active"
+        })
+      }
+    }
 })
 
