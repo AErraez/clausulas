9722fc0c1fd960a20cdca8bfe30de3f41a02b1e1
Ariel Erraez <arielon88@gmail.com>
2023-04-26 23:53:41 -0500

    Review clauses

--- bundle.js
+++ bundle.js
@@ -5636,7 +5636,7 @@ const clausulas= require('./lista_clau.json')
 const stringSimilarity = require('string-similarity');
 
 
-
+//**********************FUNCIONES********************//
 
 function findSimilarSentences(objclaus, inputSentence) {
   let similarities = [];
@@ -5662,7 +5662,23 @@ function arrtotext(arr) {
   return txt.substring(0,txt.length -1)
 }
 
+
+//**********************VARIABLES********************** */
+
 let boton= document.getElementById("submit")
+
+let reviewbutt =document.getElementById("revisar")
+
+let reviewdiv= document.getElementById("check")
+
+let activelist
+
+let clausramo
+
+let unselecteddiv= document.getElementById("unselected")
+
+
+//**************EVENTOS************************************ */
 
 boton.addEventListener("click", (event)=>{
     let ramo= document.getElementById("ramo").value
@@ -5676,7 +5692,7 @@ boton.addEventListener("click", (event)=>{
         return elem
     })
 
-    let clausramo= clausulas[ramo]
+    clausramo= clausulas[ramo]
     console.log(txtclean[0])
     let section=document.getElementById("resultado")
     let footer= document.getElementById("clausulas")
@@ -5689,7 +5705,8 @@ boton.addEventListener("click", (event)=>{
                       <div class="col-2 "><button class="claus-but active" value=${similist[0][1]}->${similist[0][0]} - ${similist[0][1]}</button></div> 
                       <div class="col-2 "><button class="claus-but" value=${similist[1][1]}->${similist[1][0]} - ${similist[1][1]}</button></div> 
                       <div class="col-2 "><button class="claus-but" value=${similist[2][1]}->${similist[2][0]} - ${similist[2][1]}</button></div> 
-                      <div class="col-md-auto text-center"><button class="claus-but" value="">X</button></div> </div>`
+                      <div class="col-md-auto text-center"><button class="claus-but" name="${element}" value="">X</button></div> </div>`
+                    
       section.innerHTML+=txttoshow
       codarray.push(similist[0][1]+"-")
     })
@@ -5726,19 +5743,45 @@ boton.addEventListener("click", (event)=>{
           boton3.but.classList.remove("active")
           boton4.but.classList.remove("active")
           boton.but.classList.add("active")
-          let activelist= document.querySelectorAll(".active")
+          activelist= document.querySelectorAll(".active") //Se crea activelist aquí ya que esta cambia con cada click del botón
           codarray=[]
           for (let elem of activelist){
             codarray.push(elem.value)
+  
           }
           codtxt=arrtotext(codarray)
           footer.innerHTML=`<div class="row py-4 justify-content-around align-items-center text-align-center"> ${codtxt}</div>`
         })
       }
     }
+    reviewdiv.style.display="block"
 })
 
+reviewbutt.addEventListener("click",(event)=>{
+  let unselectedarr= []
+  unselecteddiv.innerHTML=""
+  for(let activebutton of activelist){
+    if (activebutton.value==""){
+      unselectedarr.push(activebutton.name)
+    }
+  }
+  unselectedarr.forEach(unselected=>{
+    unselecteddiv.innerHTML+=`<div class="row py-2"><p>- ${unselected}</p></div>`
+  })
+  
 
+  let selectdiv= document.getElementById("manualclaus")
+  let filterbutton= document.getElementById("searchclaus")
+  filterbutton.addEventListener("change",(event)=>{
+    manualclaus.innerHTML=""
+    let filtertxt = filterbutton.value.toUpperCase()
+    let filteredclaus= clausramo.filter(claus => claus["Cláusula"].includes(filtertxt))
+    filteredclaus.forEach(elem=>{
+      manualclaus.innerHTML+=`<option>${elem["Código"]} - ${elem["Cláusula"]}</option>`
+    })
+  })
+  
+})
 },{"./lista_clau.json":1,"string-similarity":3}],3:[function(require,module,exports){
 module.exports = {
 	compareTwoStrings:compareTwoStrings,
--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const clausulas= require('./lista_clau.json')
 const stringSimilarity = require('string-similarity');
 
 
-
+//**********************FUNCIONES********************//
 
 function findSimilarSentences(objclaus, inputSentence) {
   let similarities = [];
@@ -28,7 +28,23 @@ function arrtotext(arr) {
   return txt.substring(0,txt.length -1)
 }
 
+
+//**********************VARIABLES********************** */
+
 let boton= document.getElementById("submit")
+
+let reviewbutt =document.getElementById("revisar")
+
+let reviewdiv= document.getElementById("check")
+
+let activelist
+
+let clausramo
+
+let unselecteddiv= document.getElementById("unselected")
+
+
+//**************EVENTOS************************************ */
 
 boton.addEventListener("click", (event)=>{
     let ramo= document.getElementById("ramo").value
@@ -42,7 +58,7 @@ boton.addEventListener("click", (event)=>{
         return elem
     })
 
-    let clausramo= clausulas[ramo]
+    clausramo= clausulas[ramo]
     console.log(txtclean[0])
     let section=document.getElementById("resultado")
     let footer= document.getElementById("clausulas")
@@ -55,7 +71,8 @@ boton.addEventListener("click", (event)=>{
                       <div class="col-2 "><button class="claus-but active" value=${similist[0][1]}->${similist[0][0]} - ${similist[0][1]}</button></div> 
                       <div class="col-2 "><button class="claus-but" value=${similist[1][1]}->${similist[1][0]} - ${similist[1][1]}</button></div> 
                       <div class="col-2 "><button class="claus-but" value=${similist[2][1]}->${similist[2][0]} - ${similist[2][1]}</button></div> 
-                      <div class="col-md-auto text-center"><button class="claus-but" value="">X</button></div> </div>`
+                      <div class="col-md-auto text-center"><button class="claus-but" name="${element}" value="">X</button></div> </div>`
+                    
       section.innerHTML+=txttoshow
       codarray.push(similist[0][1]+"-")
     })
@@ -92,15 +109,42 @@ boton.addEventListener("click", (event)=>{
           boton3.but.classList.remove("active")
           boton4.but.classList.remove("active")
           boton.but.classList.add("active")
-          let activelist= document.querySelectorAll(".active")
+          activelist= document.querySelectorAll(".active") //Se crea activelist aquí ya que esta cambia con cada click del botón
           codarray=[]
           for (let elem of activelist){
             codarray.push(elem.value)
+  
           }
           codtxt=arrtotext(codarray)
           footer.innerHTML=`<div class="row py-4 justify-content-around align-items-center text-align-center"> ${codtxt}</div>`
         })
       }
     }
+    reviewdiv.style.display="block"
 })
 
+reviewbutt.addEventListener("click",(event)=>{
+  let unselectedarr= []
+  unselecteddiv.innerHTML=""
+  for(let activebutton of activelist){
+    if (activebutton.value==""){
+      unselectedarr.push(activebutton.name)
+    }
+  }
+  unselectedarr.forEach(unselected=>{
+    unselecteddiv.innerHTML+=`<div class="row py-2"><p>- ${unselected}</p></div>`
+  })
+  
+
+  let selectdiv= document.getElementById("manualclaus")
+  let filterbutton= document.getElementById("searchclaus")
+  filterbutton.addEventListener("change",(event)=>{
+    manualclaus.innerHTML=""
+    let filtertxt = filterbutton.value.toUpperCase()
+    let filteredclaus= clausramo.filter(claus => claus["Cláusula"].includes(filtertxt))
+    filteredclaus.forEach(elem=>{
+      manualclaus.innerHTML+=`<option>${elem["Código"]} - ${elem["Cláusula"]}</option>`
+    })
+  })
+  
+})
